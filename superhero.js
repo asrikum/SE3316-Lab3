70c2099d2a70483dd6b3e1e595508e4339d49560
asrikum <114449366+asrikum@users.noreply.github.com>
2023-11-05 02:55:20 -0500

    Completed power implementation

--- superhero.js
+++ superhero.js
@@ -23,8 +23,27 @@ if(res.ok){
 
 
 // Define the function that will handle the search
-function searchSuperheroes(searchTerm, category, displayvolume) {
+function searchSuperheroes(searchTerm, category, displayvolume, searchpower) {
     console.log(`api/superheroes/search?field=${category}&pattern=${searchTerm}&n=${displayvolume}`)
+    if(category == 'power'){
+        console.log(`api/superheroes/search/power?power=${searchpower}&n=${displayvolume}`);
+        fetch(`api/superheroes/search/power?power=${searchpower}&n=${displayvolume}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('results');
+            resultsContainer.innerHTML = ''; // Clear previous results
+            data.forEach(data => {
+              const div = document.createElement('div');
+              // Update these fields to match the actual properties of your superhero objects
+              div.textContent = `Name: ${data}`;
+              resultsContainer.appendChild(div);
+            });
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+    else{
   fetch(`api/superheroes/search?field=${category}&pattern=${searchTerm}&n=${displayvolume}`)
     .then(response => response.json())
     .then(data => {
@@ -34,6 +53,7 @@ function searchSuperheroes(searchTerm, category, displayvolume) {
       console.error('Error:', error);
     });
 }
+}
 
 // Define the function that will display the results
 function displayResults(superheroes) {
@@ -42,7 +62,7 @@ function displayResults(superheroes) {
   superheroes.forEach(hero => {
     const div = document.createElement('div');
     // Update these fields to match the actual properties of your superhero objects
-    div.textContent = `Name: ${hero.name}, Race: ${hero.Race}, Publisher: ${hero.Publisher}, Power: ${hero.Power}`;
+    div.textContent = `Name: ${hero.name}`;
     resultsContainer.appendChild(div);
   });
 }
@@ -52,10 +72,12 @@ searchButton.addEventListener("click", () => {
   // Get the current value of the input and select elements
   const displayn = displayvol.value.toLowerCase();
   const searchTerm = searchTermInput.value.toLowerCase();
+  const searchpower = searchTermInput.value;
   const category = categorySelect.value;
   const displayvolume = parseInt(displayn);
-  console.log(searchTerm, category, displayvolume); // Log the search term and category for debugging
+  console.log(searchTerm, category, displayvolume, searchpower); // Log the search term and category for debugging
 
   // Call the search function with the current search term and category
-  searchSuperheroes(searchTerm, category, displayvolume);
+  searchSuperheroes(searchTerm, category, displayvolume, searchpower);
 });
+
