065ff8c1e1ffc31268627b5b5a23b48ce6986666
asrikum <114449366+asrikum@users.noreply.github.com>
2023-11-06 07:38:28 -0500

    Finished Get Fetch Request On front End and implemented search bar

--- superhero.js
+++ superhero.js
@@ -8,6 +8,8 @@ const displayvol = document.getElementById('displayvolume');
 const listName = document.getElementById('listName');
 const superhero_ids = document.getElementById('ID');
 const listresults = document.getElementById('list_results');
+const listreturn = document.getElementById('Return');
+const listreturnbutton = document.getElementById('submitreturn');
 const fetchSuperhero = async () => {
     try{
 const res = await fetch('/api/superheroes')
@@ -35,16 +37,17 @@ function createSuperheroList(listName, superheroIds) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ listName }) // Assuming the server expects an object with a key 'listName'
     };
-  
+//input Sanitization before fetching
     fetch('/api/lists', fetchList)
       .then(response => {
         if (!response.ok) {
-            
+            listresults.innerHTML='List Exists';
           throw new Error('Network response was not ok ' + response.statusText);
         }
         return response.json(); // Parse JSON response into JavaScript object
       })
       .then(data => {
+        listresults.innerHTML='List Created';
         console.log('List created:', data); // Handle the response data
   
         // Now, add superhero IDs to the list
@@ -54,11 +57,11 @@ function createSuperheroList(listName, superheroIds) {
           body: JSON.stringify({superheroIds}) // Assuming the server expects an object with a key 'superhero_ids'
         };
   console.log(JSON.stringify(superheroIds ));
-
+//input Sanitization before fetching
         return fetch(`/api/lists/${listName}`, fetchIDs); // Adjust the endpoint as per your API
       })
       .then(response => {
-        if (!response.ok) {
+        if (!response.ok) {  
           throw new Error('Network response was not ok ' + response.statusText);
         }
         return response.json();
@@ -68,27 +71,38 @@ function createSuperheroList(listName, superheroIds) {
         // Fetch the updated list
       });
   }
-function getSuperheroList(){
-    fetch(`/api/lists/${listName}`) // Adjust the endpoint as per your API
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .then(data => {
-        const resultsContainer = document.getElementById('list_results');
-        resultsContainer.innerHTML = ''; // Clear previous results
-        data.forEach(superhero => {
-          const div = document.createElement('div');
-          // Update these fields to match the actual properties of your superhero objects
-          div.innerHTML = `ID: ${superhero.id}, Name: ${superhero.name}, Gender: ${superhero.gender}, Eye Color: ${superhero['Eye color']}, Race: ${superhero.race}, Hair: ${superhero.Hair}, Height: ${superhero.Height}, Publisher: ${superhero.Publisher}, Skin: ${superhero['Skin color']}, Alignment: ${superhero.Alignment}, Weight: ${superhero.Weight}, Powers: ${superhero.powers.join(', ')}`;
-          resultsContainer.appendChild(div);
+  function getSuperheroList(listreturn) {
+    console.log(`Fetching list: ${listreturn}`); // Debug log
+    fetch(`/api/lists/${listreturn}`) // Adjust the endpoint as per your API
+        .then(response => {
+            if (!response.ok) {
+                console.error('Fetch error:', response.statusText); // Debug log
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Received data:', data); // Debug log
+            const resultsContainer = document.getElementById('list_results');
+            resultsContainer.innerHTML = ''; // Clear previous results
+
+            // Check if data is an array before attempting to use forEach
+            if (data.superheroes && Array.isArray(data.superheroes)) {
+                data.superheroes.forEach(superhero => {
+                    const div = document.createElement('div');
+                    
+                    // Update these fields to match the actual properties of your superhero objects
+                    div.innerHTML = `ID: ${superhero.id}, Name: ${superhero.name}, Gender: ${superhero.gender}, Eye Color: ${superhero.Eye_Color}, Race: ${superhero.race}, Hair: ${superhero.Hair}, Height: ${superhero.Height}, Publisher: ${superhero.Publisher}, Skin: ${superhero.Skin}, Alignment: ${superhero.Alignment}, Weight: ${superhero.Weight}, Powers: ${superhero.powers.join(', ')}`;
+                    resultsContainer.appendChild(div);
+                });
+            } else {
+                // Handle non-array data here
+                console.error('Data received is not an array:', data);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
 }
 
 
@@ -154,10 +168,15 @@ searchButton.addEventListener("click", () => {
   // Call the search function with the current search term and category
   searchSuperheroes(searchTerm, category, displayvolume, searchpower);
 });
-const createListButton = document.getElementById("createListButton");
 submitlistButton.addEventListener("click", () => {
   const listVal = listName.value;
   const ids = superhero_ids.value.split(',').map(id => parseInt(id.trim(), 10)); // Convert string of IDs into an array of numbers
   createSuperheroList(listVal, ids);
   console.log(ids)
 });
+listreturnbutton.addEventListener("click", () => {
+  const listreturnsi = listreturn.value;
+  getSuperheroList(listreturnsi);
+});
+
+
